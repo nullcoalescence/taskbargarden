81fa443818af5e665dc699950ce5fd817aa3e42a
ben <{username}@users.noreply.github.com>
2026-04-26 17:16:03 -0500

    create app

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace taskbargarden
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
